d1c87a1bb3bcb025a80c233c50706b82c8246548
Ljime <hermanvuong2@gmail.com>
2026-05-20 01:39:40 -0400

    fix dns 3

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import cors from 'cors';
 import dns from 'dns';
 dns.setDefaultResultOrder('ipv4first');
-import pg from 'pg';
-import { randomUUID } from 'crypto';
+
 import dotenv from "dotenv";
 dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import pg from 'pg';
+import { randomUUID } from 'crypto';
 
 const { Pool } = pg;
 const app = express();
